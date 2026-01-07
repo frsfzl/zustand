585d8e84178119a52e484e3b90df0b2489dda4f3
Faris Faizal <fariswonders@gmail.com>
2026-01-06 23:36:42 -0500

    Created habit shower and made it persist in local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
+import { Box, Container, Typography } from '@mui/material'
 import './App.css'
 import useHabitStore from "./store/store";
+import AddHabitForm from './components/add-habit-form';
+import HabitList from './components/habit-list';
 
 function App() {
   const store = useHabitStore();
   console.log(store);
 
-  return <div>Temporary</div>;
+  return (
+    <Container>
+      <Box>
+        <Typography variant="h2" component="h1" gutterBottom align="center">
+          Habit Tracker
+        </Typography>
+        <AddHabitForm />
+        <HabitList />
+        {/* stats */}
+      </Box>
+    </Container>
+  );
 }
 
 export default App
--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { createJSONStorage, devtools, persist } from "zustand/middleware";
 
 export interface Habit {
   id: string;
@@ -10,12 +11,49 @@ export interface Habit {
 
 interface HabitState {
   habits: Habit[];
+  addHabit: (name: string, frequency: "daily" | "weekly") => void;
+  removeHabit: (id: string) => void;
+  toggleHabit: (id: string, date: string) => void;
 }
 
-const useHabitStore = create<HabitState>()(()=>{
+const useHabitStore = create<HabitState>()(devtools(persist((set) => {
   return {
-      habits: [],
-    };
-});
+    habits: [],
+    addHabit: (name, frequency) => set((state) => {
+      return {
+        habits: [
+          ...state.habits,
+          {
+            id: Date.now().toString(),
+            name,
+            frequency,
+            completedDates: [],
+            createdAt: new Date().toISOString(),
+          },
+        ],
+      };
+    }),
+    removeHabit: (id) => set((state) => ({
+      habits: state.habits.filter((habit) => habit.id !== id),
+    })),
+    toggleHabit: (id, date) =>
+      set((state) => ({
+        habits: state.habits.map((habit) =>
+          habit.id === id ? {
+            ...habit,
+            completedDates: habit.completedDates.includes(date)
+              ? habit.completedDates.filter((d) => d !== date)
+              : [...habit.completedDates, date],
+          }
+            : habit
+        ),
+      })),
+  };
+},
+  {
+    name: 'habits-local',
+    storage: createJSONStorage(() => localStorage),
+  })
+));
 
 export default useHabitStore;
